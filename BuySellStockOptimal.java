8eed9674aea44333cc342fc9bbcfa760d181676f
govind <govindrajingle@gmail.com>
2025-01-05 20:23:59 +0530

    day 3

---
+++ BuySellStockOptimal.java
@@ -0,0 +1,12 @@
+public class BuySellStockOptimal {
+    public static void main(String[] args) {
+        int prices [] = {7, 1, 5, 3, 1, 4};
+        int n = prices.length, bestBuy = Integer.MAX_VALUE, maxProfit = Integer.MIN_VALUE;
+        // bestBuy is variable to find the day at we should buy the stock to get maximum profit
+        for(int i=0; i<n; i++){
+            bestBuy = Math.min(bestBuy, prices[i]);
+            maxProfit = Math.max(maxProfit, prices[i]-bestBuy);
+        }
+        System.out.println(maxProfit);
+    }
+}
