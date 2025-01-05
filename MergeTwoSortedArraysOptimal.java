7242576c66f9ea872cd0ce4e3be40e563dc32b7b
govind <govindrajingle@gmail.com>
2025-01-06 00:35:50 +0530

    day 4

---
+++ MergeTwoSortedArraysOptimal.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class MergeTwoSortedArraysOptimal {
+    public static void main(String[] args) {
+        int a[] = {1, 3, 6, 0, 0, 0};  // Array a
+        int b[] = {2, 5, 7};           // Array b
+        int m = a.length - 1, n = b.length;
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while(j >= 0) {
+            if(i >= 0 &&  a[i] > b[j]){
+                a[k--] = b[i--];
+            } else {
+                a[k--] = b[j--];
+            }
+        }
+    }
+}
