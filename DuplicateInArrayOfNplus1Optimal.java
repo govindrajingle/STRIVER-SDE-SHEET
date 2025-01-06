f8e6a7f250b86abc4ebb939e575de3543ce190b9
govind <govindrajingle@gmail.com>
2025-01-06 21:35:03 +0530

    day 5

---
+++ DuplicateInArrayOfNplus1Optimal.java
@@ -0,0 +1,12 @@
+public class DuplicateInArrayOfNplus1Optimal {
+    public static void main(String[] args) {
+        int a[] = {1, 2, 3, 4, 6, 6}; // Input array with a duplicate element
+        int slow = a[0], fast = a[0];
+        // 1. Detect starting point of cycle in linked list
+        do{
+            slow = a[slow];
+            fast = a[a[fast]];
+        } while (slow != fast);
+        System.out.println("Duplicate element: " + slow);
+    }
+}
