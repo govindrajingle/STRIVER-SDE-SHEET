f75aabfe2bc4532caeef7a1caf1074e775b45ea8
govind <govindrajingle@gmail.com>
2025-01-11 15:59:41 +0530

    day 8

---
+++ MajorityElementOptimal.java
@@ -0,0 +1,28 @@
+public class MajorityElementOptimal {
+    public static void main(String[] args) {
+        int arr [] = {3, 2, 1, 2, 2, 4, 2};
+        //Moore's voting algorithm
+        int freq = 0, element = arr[0],  n = arr.length;
+        for(int i=0; i<n; i++){
+            if(freq == 0){
+                element = arr[i];
+            } if (arr[i] == element){
+                freq++;
+            } else {
+                freq--;
+            }
+        }
+        int count = 0;
+        for(int i : arr){
+            if(i == element) {
+                count++;
+            }
+        }
+        if(count > n/2){
+            System.out.println("majority element is " + element);
+        } else {
+            System.out.println("majority element does not exist");
+        }
+
+    }
+}
