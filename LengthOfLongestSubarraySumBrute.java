7e15e8b871e5a2db0a5a59b702fc2b7764a677e6
Govindraj <inglesuresh@cdacnoida.in>
2025-03-24 17:30:34 +0530

    day 11

---
+++ LengthOfLongestSubarraySumBrute.java
@@ -0,0 +1,17 @@
+public class LengthOfLongestSubarraySumBrute {
+    public static void main(String[] args) {
+        int arr [] = {6, -2, 2, -8, 1, 7, 4, -10};
+        int n = arr.length;
+        int max = 0;
+        for(int i=0; i<n; i++){
+            int sum = 0;
+            for(int j=i; j<n; j++){
+                sum += arr[j];
+                if(sum == 0) {
+                    max = Math.max(max, j - i + 1);
+                }
+            }
+        }
+        System.out.println("Maximum length sub array = " + max);
+    }
+}
